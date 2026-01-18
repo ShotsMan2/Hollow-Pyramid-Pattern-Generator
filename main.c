e3f452a9c3d226c2187a10990a5a946f1470a02c
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 23:11:53 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,28 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-     int i,j,k,satir,m;
+    int i, j, k, satir, m;
 
-    satir=5;
+    satir = 5;
 
-    for(i=1;i<=satir;i++){
-        for(k=1;k<=satir-i;k++){
+    for (i = 1; i <= satir; i++)
+    {
+        for (k = 1; k <= satir - i; k++)
+        {
             printf(" ");
         }
-        if(i==1){
+        if (i == 1)
+        {
             printf("*");
         }
-        else{
+        else
+        {
             printf("*");
-            for(j=1;j<=2*i-3;j++){
+            for (j = 1; j <= 2 * i - 3; j++)
+            {
                 printf(" ");
             }
             printf("*");
